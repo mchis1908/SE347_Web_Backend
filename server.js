61d6e39becdee99b289f25a953684baa266299e1
mchis1908 <mchis19.08@gmail.com>
2023-04-15 01:39:02 +0700

    setup backend

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// Định nghĩa tuyến đường cho trang chủ
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
