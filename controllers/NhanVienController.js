4beea0292445883a670674d5335329ed54b318df
mchis1908 <mchis19.08@gmail.com>
2023-05-08 01:13:56 +0700

    updated

--- controllers/NhanVienController.js
+++ controllers/NhanVienController.js
@@ -18,12 +18,21 @@ const NhanVien = {
             res.status(500).json(error)
         }
     },
+    GetNhanVienbysdt: async (req, res) => {
+      const { sdt } = req.params;
+      try {
+          const NV = await NhanVienModel.find({ SDT: sdt });
+          res.send(NV);
+      } catch (error) {
+          res.status(502).send(error);
+      }
+  },
     UpdateNhanVien: async (req, res) => {
         const { sdt } = req.params;
         try {
-            const KH = await NhanVienModel.findOneAndUpdate({ SDT: sdt }, req.body);
-            await KH.save();
-            res.send(KH);
+            const NV = await NhanVienModel.findOneAndUpdate({ SDT: sdt }, req.body);
+            await NV.save();
+            res.send(NV);
         } catch (error) {
             res.status(502).send(error);
         }
--- routes/HoaDonRoute.js
+++ routes/HoaDonRoute.js
@@ -4,8 +4,10 @@ const router = require('express').Router()
 router.post('/themhoadon', HoaDonController.DangKyHoaDon);
 router.get('/gethoadon', HoaDonController.GetHoaDon)
 router.get('/gethoadon/:maHD', HoaDonController.GetHoaDon_bymahd)
+router.get('/gethoadonbanhangbytg/:tg', HoaDonController.GetHoaDonBanHang_bythoigian)
 router.delete('/deletehoadon/:id', HoaDonController.DeleteHoaDon)
 router.patch('/updatehoadon/:maHD', HoaDonController.UpdateHoaDon)
 router.get('/searchhoadon/:mahd', HoaDonController.SearchHoaDon);
+router.patch('/updatehoadon1', HoaDonController.UpdateHoaDon1)
 
 module.exports = router
